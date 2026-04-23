5b25c320106a65dd3a55a23313d253dc8b4ece1b
Airton Frazon <contato@frazon.com.br>
2026-04-23 14:50:12 -0300

    feat: implement initial product fetch logic using async/await

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log('teste de js');
+let nextUrl = 'https://desafio-api.bold.workers.dev/products?page=1'
+
+
+async function fetchProducts(){
+    try{
+        const response = await fetch(nextUrl);
+        const data = await response.json();
+
+        console.log('Lista de produtos', data);
+
+    } catch (exception){
+        console.error("Erro ao carregar produtos:", error);
+    }
+}
+
+fetchProducts();
